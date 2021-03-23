ec980e1909a5205f5b45dedf35e89d297e125250
Eric <57415580+twojineric@users.noreply.github.com>
2021-03-22 21:22:06 -0700

    fix

--- test.js
+++ test.js
@@ -3,11 +3,13 @@ const jsdom = require("jsdom");
 const fs = require('fs');
 const parseString = require('xml2js').parseString;
 const { JSDOM } = jsdom;
+require('dotenv').config();
 
-//getBill("https://api.govinfo.gov/packages/BILLS-117hr1319enr/htm?api_key=lu3NgrN5aGrgLEvHhqO3Nhue7PiCrRwdnCMr2sCe");
+//getBill("https://api.govinfo.gov/packages/BILLS-117hr1319enr/htm?api_key=process.env.GOVINFO_KEY");
 //textAndLink("https://uscode.house.gov/view.xhtml?hl=false&edition=prelim&req=granuleid%3AUSC-2016-title2-section1954&num=0");
-getBill("https://www.govinfo.gov/content/pkg/USCODE-2015-title26/html/USCODE-2015-title26-subtitleA-chap1-subchapA-partII-sec11.htm");
+getBillHTML("https://www.govinfo.gov/content/pkg/USCODE-2015-title26/html/USCODE-2015-title26-subtitleA-chap1-subchapA-partII-sec11.htm");
 //getBill("http://uscode.house.gov/quicksearch/get.plx?title=26&section=280F");
+getXMLURL("hr1", 115);
 
 //gets the url leading to the text of the bill provided. Prints the latest version.
 //ex HR 3298 from 115th would be ("hr3298", 115)
@@ -15,7 +17,7 @@ async function getXMLURL(billid, congress)
 {
     let url = `https://api.propublica.org/congress/v1/${congress}/bills/${billid}.json`
     await fetch(url, {
-        headers: {"X-API-Key": "4FGVYsQBIq2xC5fjWcNSMH3QszIi3y6S6BpHjD08"}
+        headers: {"X-API-Key": process.env.PROPUBLICA_KEY}
     })
     .then(res => res.json())
     .then(body => console.log(body.results[0].versions[0].url));
