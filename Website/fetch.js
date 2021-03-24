0d1c3132625e7c75e5028f4b109f0f4aa04ba5cb
Jerry Lu <jclu2gm@gmail.com>
2021-03-23 22:45:53 -0700

    Everything but Functions
    Everything but the function calls are implemented.

--- Website/fetch.js
+++ Website/fetch.js
@@ -1,8 +1,9 @@
-var nodesAdded = 0;
 var lenBefore = 0;
+var lenBefore2 = 0;
+var lenBefore3 = 0;
 
 function getLink() {            
-    clear();
+    clear(lenBefore);
     
     // Get Input Values
     var h1, h2;
@@ -47,34 +48,76 @@ function getLink() {
             link = document.createTextNode(info[i].status);
             element.appendChild(link);
             element.title = info[i].status;
-            element.addEventListener("click",search,false);
+            element.addEventListener("click",searchSetup,false);
             element.myParam=info[i].url;
-            element.style.width='150px';
+            element.style.width="150px";
             document.body.appendChild(element);
             document.body.appendChild(br);
         }
     })     
     
-    function clear() {
-        while(lenBefore != 0 && document.body.childNodes.length > lenBefore) {
-            document.body.removeChild(document.body.childNodes[lenBefore]);
+    function clear(length) {
+        while(length != 0 && document.body.childNodes.length > length) {
+            document.body.removeChild(document.body.childNodes[length]);
         }
     }
 
-    function search(event) {
+    // Create input box fields and submit button
+    function searchSetup(event) {
         var xml = event.currentTarget.myParam;
-        
-        
-        
-        /*
-        fetch(xml, {mode : 'no-cors'})
-        .then(res => res.text())
-        .then(function(data) {
-            var json = xmlToJson(new DOMParser().parseFromString(data, 'text/xml'));
-            console.log(json);
-        })
 
+        // Debugging
         document.getElementById("p3").innerHTML=xml;
-        */
+
+        var json = "EricFunction()";
+
+        clear(lenBefore2);
+        lenBefore2 = document.body.childNodes.length;
+
+        document.body.appendChild(document.createElement("br"));
+        var para1 = document.createElement("p");
+        para1.innerHTML = "<b>Input Section Number</b>";
+        document.body.appendChild(para1);
+
+        var input1 = document.createElement("input");
+        input1.type = "text";
+        input1.id = "inp1";
+        input1.placeholder = "1";
+        document.body.appendChild(input1);
+        document.body.appendChild(document.createElement("br"));
+
+        // Remove this input later
+        var input2 = document.createElement("input");
+        input2.type = "text";
+        input2.id = "inp2";
+        input2.placeholder = "Dump Text Here";
+        document.body.appendChild(input2);
+        document.body.appendChild(document.createElement("br")); 
+
+        var button1 = document.createElement("button");
+        button1.appendChild(document.createTextNode("Submit"));
+        button1.title = "Submit";
+        button1.addEventListener("click",search,false);
+        document.body.append(button1);
+    }
+
+    function search() {
+        clear(lenBefore3);
+        var section,text;
+        section = document.getElementById("inp1").value;
+        text = document.getElementById("inp2").value;
+
+        // IainFunction(section,text);
+
+        var output = "ReplaceThis";
+        lenBefore3 = document.body.childNodes.length;
+
+        var para1 = document.createElement("p");
+        para1.innerHTML = `<b>Section ${section}:</b>`;
+        document.body.appendChild(para1);
+
+        var outNode = document.createElement("p");
+        outNode.innerText = output;
+        document.body.appendChild(outNode);  
     }
 }
