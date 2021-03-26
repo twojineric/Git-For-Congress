211ac2596c323f1493572cfed7db9b9272c9dba1
Jerry Lu <jclu2gm@gmail.com>
2021-03-26 10:57:27 -0700

    Works with updated search function
    Parts of section input are case insensitive and multiple delimiters work.

--- Website/fetch.js
+++ Website/fetch.js
@@ -1,9 +1,10 @@
 var lenBefore = 0;
 var lenBefore2 = 0;
 var lenBefore3 = 0;
-var outNode;
+var warning = true;
+var ident;
 
-function getLink() {            
+async function getLink() {            
     lenBefore2 = 0;
     lenBefore3 = 0;
     clear(lenBefore);
@@ -15,6 +16,23 @@ function getLink() {
     h1 = h1.split(" ").join("");
     h2 = document.getElementById("head2").value.trim();
     
+    // URL Manipulation to get the id
+    document.getElementById("head1").addEventListener("input",warnTrue);
+    document.getElementById("head2").addEventListener("input",warnTrue);
+    document.getElementById("head3").addEventListener("input",warnTrue);
+    var h3 = document.getElementById("head3").value.trim();
+    var pound = h3.indexOf("#");
+    var bill = h3.indexOf("/"+h2);
+    var cong = h3.indexOf("/"+h1.replace(/\D/g, "")+"/");
+    if(pound ==  -1 || bill == -1 || cong == -1) {
+        if(warning) {
+            alert("Bill and/or congress don't match URL");
+            warning = false;
+            return;
+        }        
+    }
+    ident = h3.substring(pound+1);
+
     // Concatenate URL and Inputs
     var url = "https://api.propublica.org/congress/v1/";
     url = url.concat(h2, "/bills/");
@@ -24,7 +42,7 @@ function getLink() {
     
     lenBefore = document.body.childNodes.length;    
 
-    fetch(url, {
+    await fetch(url, {
         headers: {"X-API-Key": API_KEY}
     })
     .then(res => res.json())
@@ -32,7 +50,7 @@ function getLink() {
         console.log(data);
 
         if(data.results == null) {
-            document.getElementById("p1").innerHTML="Input Valid Bill ex. hr115, 115";
+            document.getElementById("p1").innerHTML="Input Valid Bill ex. hr1, 115, " + exURL;
             document.getElementById("p2").innerHTML="";
             return;
         }
@@ -41,8 +59,9 @@ function getLink() {
         var i;
         var element;
         var link;
-        document.getElementById("p2").innerHTML="<b>"+data.results[0].bill + 
-            " Congress " + data.results[0].congress+"<b>";
+        let year = data.results[0].latest_major_action_date.split('-')[0];
+        document.getElementById("p2").innerHTML="<b>"+data.results[0].bill +
+            " Congress " + data.results[0].congress + " - " + year +"<b>";
 
         // Create buttons
         for(i = 0; i < info.length; i++) {
@@ -53,12 +72,13 @@ function getLink() {
             element.title = info[i].status;
             element.addEventListener("click",searchSetup,false);
             element.myParam=info[i].url;
+            element.yr=year;
             element.id = "elem1";
             element.style.width="150px";
             document.body.appendChild(element);
             document.body.appendChild(br);
         }
-    })     
+    })  
     
     function clear(length) {
         while(length != 0 && document.body.childNodes.length > length) {
@@ -66,28 +86,52 @@ function getLink() {
         }
     }
 
+    function warnTrue() {
+        warning = true;
+    }
+
     // Create input box fields and submit button
-    async function searchSetup(event) {
+    async function searchSetup()
+    {
         var xml = document.getElementById("elem1").myParam;
-        var jXml;
+        var jXml,uscText;
         clear(lenBefore2);
         lenBefore2 = document.body.childNodes.length;
 
-        let j = { url: xml };
-                console.log(j);
-                await fetch("http://localhost:3000/getxml", {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(j)
-                })
-                .then(resp => resp.json())
-                .then(body => {
-                    let e = document.createElement('p');
-                    e.textContent = JSON.stringify(body.file.bill);
-                    jXml = e.textContent;
-                });
+        element = document.createElement("p");
+        document.body.appendChild(element);
+        
+        let j = {
+            url: xml,
+            id: ident,
+            year: document.getElementById("elem1").yr
+        };
+        await fetch("http://localhost:3000/getxml", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(j)
+        })
+        .then(resp => resp.json())
+        .then(async body => {
+            let e = document.createElement('p');
+            e.textContent = body.txt;
+            jXml = e.textContent;
+            document.body.appendChild(e);
+
+            await fetch("http://localhost:3000/getUSC", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body.usc)
+            })
+            .then(resp => resp.json())
+            .then(body => {
+                uscText = body.file;
+            })
+        });
 
         document.body.appendChild(document.createElement("br"));
         var para1 = document.createElement("p");
@@ -97,42 +141,16 @@ function getLink() {
         var input1 = document.createElement("input");
         input1.type = "text";
         input1.id = "inp1";
-
-        // Change this back to 1
-        input1.placeholder = "Command";
+        input1.placeholder = "Section";
+        input1.myParam = jXml;
         document.body.appendChild(input1);
-        document.body.appendChild(document.createElement("br"));
-
-
-        // User input
-        var input2 = document.createElement("input");
-        input2.type = "text";
-        input2.id = "inp2";
-        input2.placeholder = "Target";
-        document.body.appendChild(input2);
-        document.body.appendChild(document.createElement("br"));
-
-        var input3 = document.createElement("input");
-        input3.type = "text";
-        input3.id = "inp3";
-        input3.placeholder = "Replacement";
-        document.body.appendChild(input3);
-        document.body.appendChild(document.createElement("br"));
-
-        var input4 = document.createElement("input");
-        input4.type = "text";
-        input4.id = "inp4";
-        input4.placeholder = "Dump Text Here";
-        document.body.appendChild(input4);
-        document.body.appendChild(document.createElement("br"));
-        // May delete later
-        
+        document.body.appendChild(document.createElement("br"));        
 
         var button1 = document.createElement("button");
         button1.appendChild(document.createTextNode("Submit"));
         button1.title = "Submit";
         button1.id = "but1";
-        button1.myParam = jXml;
+        button1.myParam = uscText;
         button1.addEventListener("click",search,false);
         document.body.append(button1);
     }
@@ -141,28 +159,18 @@ function getLink() {
         clear(lenBefore3);
         vals = document.getElementById("inp1").value;
         json = document.getElementById("but1").myParam;
+        var changes = document.getElementById("inp1").myParam;
         lenBefore3 = document.body.childNodes.length;
 
         var para1 = document.createElement("p");
-        para1.innerHTML = `<b>Section ${vals}:</b>`;
+        var outNode = document.createElement("p");
+        outNode.innerHTML = parseSectionText();
+        para1.innerHTML = `<b>Section ${section}:</b>`;
         document.body.appendChild(para1);
-        outNode = document.createElement("p");
-        
-        /*
-        Uncomment this later
-        // updateVariables();
-        parseSection();
-        */
         
         
         var command,target,replaceWith,temp;
-
-        // Take User Input
-        command = document.getElementById("inp1").value;
-        target = document.getElementById("inp2").value;
-        replaceWith = document.getElementById("inp3").value;
-        outNode.innerHTML = document.getElementById("inp4").value;        
-        // May Remove Later
+        command = 0;
         
         if(command == 1) {
             // Strike and Insert
@@ -186,6 +194,7 @@ function getLink() {
         }
         else if (command == 4) {
             // Amend an entire section
+            outNode.innerHTML = outNode.innerHTML.replaceAll(target,replaceWith);
         }
         else if (command == 5) {
             // Append new no target
--- Website/test.js
+++ Website/test.js
@@ -1,10 +1,61 @@
-let json = 0; 
+let json; 
 let vals;
+let section;
+
+function parseSectionText()
+{
+    let object = json;
+    console.log(object);
+
+    // Any delimiters work and 1st and 3rd characters are case insensitive
+    section = vals.trim().split(",").join("_").split(" ").join("_");
+    if(section.length > 5) {
+        section = section.charAt(0).toLowerCase() + section.substring(1,4) 
+            + section.charAt(4).toUpperCase() + section.slice(5);
+    }
+    else if(section.length == 5) {
+        section = section.charAt(0).toLowerCase() + section.substring(1,4) 
+            + section.charAt(4).toUpperCase();
+    }
+    else if(section.length > 1) {
+        section = section.charAt(0).toLowerCase() + section.slice(1);
+    }
+    else if(section.length == 1) {
+        section = section.toLowerCase();
+    }
+
+    let location = 'substructure-location' + "_" + section;
+
+
+    let origLoc = location;
+    let index = object.indexOf(location);
+    
+
+    let nextIndex = object.indexOf(location, index+1);
+    let statement = '';
+    while(object.indexOf(origLoc, index+1) != -1 && object.charAt(object.indexOf(origLoc, index+1) + origLoc.length) == '_')
+    {
+        statement += json.substring(index + location.length, nextIndex);
+        index = nextIndex;
+        location = object.substring(index, object.indexOf('(', index+1));
+        nextIndex = object.indexOf(origLoc, index+1);
+    }
+    
+
+    nextIndex = json.indexOf('substructure-location', index+1);
+    if(nextIndex == -1)
+    {
+        nextIndex = object.indexOf('sourcecredit');
+    }
+    
+    statement += json.substring(index + location.length, nextIndex);
+
+    return statement;
+}
 
 function parseSection()
 {
     let object = JSON.parse(json);
-    console.log(object);
     //let section = object['legis-body']['section']['enum'[vals[1].value - 1]];
     
     //format for normal json props
@@ -15,7 +66,7 @@ function parseSection()
 
     //let test = JSON.stringify(object);
 
-    let section = vals;
+    let section = vals[0].value;
     //let subsection = vals[1].value;
 
     /*
@@ -23,7 +74,51 @@ function parseSection()
         subsection = subsection.charCodeAt(0) - 97;
     */
     
-    let tester = object['legis-body'][0]['section'][section-1];
+    let sections = object.bill['legis-body'][0];
+    
+    findSection(sections, section);
+
+    /*
+    if(sections['title'])
+    {
+        for(let i = 0; i < sections['title'].length; i++)
+        {
+            let titleSections = sections['title'][i];
+            for(let j = 0; j < titleSections['section'].length; j++)
+            {
+                if(titleSections['section'][j]['enum'] == section + '.')
+                {
+                    var index = j;
+                    var titleNum = i;
+                }    
+            }   
+        }
+    }
+    else
+    {
+        for(let i = 0; i < sections['section'].length; i++)
+        {
+            //console.log(sections['section'][i]['enum'][0]);
+
+            if(sections['section'][i]['enum'] == section + '.')
+            {
+                var index = i;
+            }       
+        }
+    }
+*/
+
+    //console.log(count);
+    //console.log(sections['section'].length);
+    let tester;
+    if(sections['title'])
+    {
+        tester = object.bill['legis-body'][0]['title'][titleNum]['section'][index];
+    }
+    else
+    {
+        let tester = object.bill['legis-body'][0]['section'][index];
+    }
 
     /*
     if(tester['subsection'] != null || subsection != null)
@@ -31,20 +126,55 @@ function parseSection()
         tester = tester['subsection'][subsection];
     }
     */
-    outNode.textContent = '';
+    document.getElementById('test').textContent = '';
 
     let values = Object.values(tester);
     
     for(let i = 1; i < values.length; i++)
     {
         if(typeof values[i] != 'object')
-            outNode.textContent += values[i] + ' ';
+            document.getElementById('test').textContent += values[i] + ' ';
         else
         {
             let obj = values[i];
             printingObjects(obj);
         }
     }
+}
+
+function findSection(testObj, secNumber)
+{
+    let currentSection = testObj;
+    let finalSection;
+
+    let currentKeys = Object.keys(testObj);
+    let currentVals = Object.values(testObj);
+    let check = false;
+
+    let returnVal = 0;
+
+    for(let i = 0; i < currentKey.length; i++)
+    {
+        if(currentKey[i] = 'section')
+        {
+            currentSection = currentSection[currentKey[i]][i];
+            returnVal[currentKey[i]] = curentKey[i];
+            for(let j = 0; j < currentSection['section'].length; j++)
+            {
+                if(currentSection['section'][j]['enum'] == secNumber)
+                {
+                    
+                    return returnVal['sectionNum'] = secNumber;
+                }
+            }
+        }
+        else
+        {
+
+        }
+    }
+    
+    
 }
 
 function printingObjects(testObj)
@@ -58,18 +188,18 @@ function printingObjects(testObj)
         if(typeof testVals[i] != 'object')
         {
             if(testKeys[i] == 'quote')
-                outNode.textContent += '"';
-            outNode.textContent += testVals[i] + ' ';
+                document.getElementById('test').textContent += '"';
+            document.getElementById('test').textContent += testVals[i] + ' ';
             if(testKeys[i] == 'quote')
-                outNode.textContent += '"';
+                document.getElementById('test').textContent += '"';
         }
         else
         {
             if(testKeys[i] == 'quote')
-                outNode.textContent += '"';
+                document.getElementById('test').textContent += '"';
             printingObjects(testVals[i]);
             if(testKeys[i] == 'quote')
-                outNode.textContent += '"';
+                document.getElementById('test').textContent += '"';
         }
     }
 
