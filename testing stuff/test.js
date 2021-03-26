af5028ab57478fc69ca129dacaebc95cbb085174
ibro912 <ibro@ucsd.edu>
2021-03-25 18:26:30 -0700

    parsing text file all subsections

--- testing stuff/test.js
+++ testing stuff/test.js
@@ -19,19 +19,49 @@ document.getElementById('inputfile').addEventListener('change', function() {
 
 function parseSectionText()
 {
-    let object = json;
+    let object = json.toLowerCase();
     let section = vals[0].value;
 
-    let location = 'substructure-location'
+    let location = 'substructure-location';
     for(let i = 0; i < section.length; i++)
     {
-        location += '_' + section.charAt(i);
+        location += '_' + section.charAt(i).toLowerCase();
+        /*
+        if(i > 0)
+            location += '_' + section.charAt(i).toUpperCase();
+        else if(i == 0)
+            location += '_' + section.charAt(i).toLowerCase();
+        else
+            location += '_' + section.charAt(i);
+        */
     }
+
+    let origLoc = location;
     let index = object.indexOf(location);
-    let nextIndex = object.indexOf('substructure-location', index+1);
-    let statement = object.substring(index + location.length, nextIndex);
-    document.getElementById('test').textContent = statement;
+    
+
+    let nextIndex = object.indexOf(location, index+1);
+    let statement = '';
+    while(object.indexOf(origLoc, index+1) != -1 && object.charAt(object.indexOf(origLoc, index+1) + origLoc.length) == '_')
+    {
+        statement += json.substring(index + location.length, nextIndex);
+        index = nextIndex;
+        location = object.substring(index, object.indexOf('(', index+1));
+        nextIndex = object.indexOf(origLoc, index+1);
+    }
+    
+
+    nextIndex = json.indexOf('substructure-location', index+1);
+    if(nextIndex == -1)
+    {
+        nextIndex = object.indexOf('sourcecredit');
+    }
+    
+    statement += json.substring(index + location.length, nextIndex);
+
+    document.getElementById('test').innerHTML = statement;
 }
+
 function parseSection()
 {
     let object = JSON.parse(json);
@@ -54,20 +84,50 @@ function parseSection()
     */
     
     let sections = object.bill['legis-body'][0];
-    var count = 0;
-    for(let i = 0; i < sections['section'].length; i++)
-    {
-        //console.log(sections['section'][i]['enum'][0]);
+    
+    findSection(sections, section);
 
-        if(sections['section'][i]['enum'] == section + '.')
+    /*
+    if(sections['title'])
+    {
+        for(let i = 0; i < sections['title'].length; i++)
         {
-            var index = i;
-        }       
+            let titleSections = sections['title'][i];
+            for(let j = 0; j < titleSections['section'].length; j++)
+            {
+                if(titleSections['section'][j]['enum'] == section + '.')
+                {
+                    var index = j;
+                    var titleNum = i;
+                }    
+            }   
+        }
     }
+    else
+    {
+        for(let i = 0; i < sections['section'].length; i++)
+        {
+            //console.log(sections['section'][i]['enum'][0]);
+
+            if(sections['section'][i]['enum'] == section + '.')
+            {
+                var index = i;
+            }       
+        }
+    }
+*/
 
     //console.log(count);
     //console.log(sections['section'].length);
-    let tester = object.bill['legis-body'][0]['section'][index];
+    let tester;
+    if(sections['title'])
+    {
+        tester = object.bill['legis-body'][0]['title'][titleNum]['section'][index];
+    }
+    else
+    {
+        let tester = object.bill['legis-body'][0]['section'][index];
+    }
 
     /*
     if(tester['subsection'] != null || subsection != null)
@@ -89,6 +149,41 @@ function parseSection()
             printingObjects(obj);
         }
     }
+}
+
+function findSection(testObj, secNumber)
+{
+    let currentSection = testObj;
+    let finalSection;
+
+    let currentKeys = Object.keys(testObj);
+    let currentVals = Object.values(testObj);
+    let check = false;
+
+    let returnVal = 0;
+
+    for(let i = 0; i < currentKey.length; i++)
+    {
+        if(currentKey[i] = 'section')
+        {
+            currentSection = currentSection[currentKey[i]][i];
+            returnVal[currentKey[i]] = curentKey[i];
+            for(let j = 0; j < currentSection['section'].length; j++)
+            {
+                if(currentSection['section'][j]['enum'] == secNumber)
+                {
+                    
+                    return returnVal['sectionNum'] = secNumber;
+                }
+            }
+        }
+        else
+        {
+
+        }
+    }
+    
+    
 }
 
 function printingObjects(testObj)
